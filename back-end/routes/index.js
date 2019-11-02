4144bf75ea816b8efd4852d3865cc50c5590af8e
RyChrome <dilsy3@gmail.com>
2019-11-02 17:41:12 -0400

    necessary

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, World');
+  res.send('https://cse.buffalo.edu/~kds/interests.html');
 });
 
 module.exports = router;
