dabc654e6fe8d07cb61ff7265a8524a2a13d0b91
Darren Matthew <darren.j.matthew@gmail.com>
2019-11-03 00:03:19 -0400

    pulled in colins work

--- back-end/src/models/auth.js
+++ back-end/src/models/auth.js
@@ -3,12 +3,32 @@ var bcrypt = require("bcrypt")
 
 class Auth {
     async signIn(req, res){
-        res.status(200).send("Sign in");
+        var username = req.body.username
+        var password = req.body.password
+
+        var success = verifyUser(username, password);
+        
+        if (success) {
+            res.status(200).send("You're in!");
+        } else {
+            res.status(400).send("Wrong username or password D:");
+        }
     }
 
+    
     async signUp(req, res){
         res.status(200).send("Sign up");
     }
+}
+
+verifyUser = async (username, password) => {
+    if(!password || !username) return false;
+
+    var user = await db("boombox").collection("users").findOne({username: username})
+
+    if(!user) return false
+
+    return bcrypt.compareSync(password, user.password);
 }
 
 module.exports = new Auth();
