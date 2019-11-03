427e0d0bb659ba1425198da6a7be71199e934651
RyChrome <dilsy3@gmail.com>
2019-11-02 21:34:52 -0400

    bad boys method

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+var router = express.Router();
+var promise = require('request-promise-native');
  
 app.use(function (req, res, next) {
   console.log('middleware');
@@ -20,11 +22,33 @@ app.ws('/', function(ws, req) {
   console.log('socket', req.testing);
 });
 
-app.ws('/police', function(ws, req) {
-  ws.on('The Police are here!', function(msg) {
-    console.lgo(msg);
-  });
-  console.log('police', req.testing)
+/**
+ * Bad boys, bad boys, whatcha gonna do? Whatcha gonna do when they come for you?
+ */
+app.put('/user', function (req, res) {
+  var options = {
+    method: 'POST',
+    uri: 'https://api.spotify.com/v1/me/player/play',
+    body: {
+      context_uri: "spotify:album:0zLd8jpRt4m6FWCu81Fb9n",
+      offset: {
+      position: 0
+    },
+      position_ms: 0,
+    },
+    json: true // Automatically stringifies the body to JSON
+  };
+});
+
+router.get('/playlists', async (req,res) => {
+  try {
+    var result = await spotifyApi.getUserPlaylists();
+    console.log(result.body);
+    res.status(200).send(result.body);
+  } catch (err) {
+    res.status(400).send(err)
+  }
+
 });
 
 app.listen(3000);
