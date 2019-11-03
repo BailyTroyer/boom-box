c5806030032a2edbb4e7ae8dcd7d2eac1abd0b86
Colin Ackerman <Cackerman55@gmail.com>
2019-11-03 01:59:03 -0500

    Added vote flesh

--- back-end/src/models/vote.js
+++ back-end/src/models/vote.js
@@ -3,7 +3,24 @@ var bcrypt = require("bcrypt")
 
 class Vote {
     async voteForSong(req, res){
-        res.status(200).send("Vote for song");
+        var { party_code, user_id, song_id } = req.body
+
+        client.connect(async (err, cli) => { 
+            const db =  cli.db("boom-box")
+            const curr_songs = await db.collection("parties").findAndModify(
+                query: {party_code: party_code}, 
+                update: {$inc: {song_queue.$[elem].votes: 1} },
+                arrayFilters: [{"elem.song_id": song_id  } ]
+            })
+
+            if (result.result.nModified === 1) {
+                res.status(200).send("Upvoted");
+            } else {
+                res.status(400).send("Something fucked up");
+            }
+        });
+
+        client.close();
     }
 }
 
